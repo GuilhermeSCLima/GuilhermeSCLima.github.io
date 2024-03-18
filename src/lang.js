4e0b06629aea5a656a8b958066dea2dbca814ade
Guilherme Lima <contato@guilhermelima.dev>
2024-03-18 19:59:54 -0300

    update on responsivel home and language change on other pages

--- src/lang.js
+++ src/lang.js
@@ -1,6 +1,11 @@
 const langs = {
-    "Portuguese": {
-      "home": "Gostaríamos de informar que nosso site está passando por um processo de construção e aprimoramento. Estamos trabalhando arduamente nos bastidores para criar uma plataforma online que atenda às suas expectativas e necessidades da melhor forma possível.",
+  "Portuguese": {
+    "home": {
+      "title": "Olá eu sou Guilherme Lima!!",
+      "text": "Gostaríamos de informar que nosso site está passando por um processo de construção e aprimoramento. Estamos trabalhando arduamente nos bastidores para criar uma plataforma online que atenda às suas expectativas e necessidades da melhor forma possível."
+    },
+    "services": {
+      "title": "Descrição dos Serviços:",
       "services": [
         "<span class=\"font-medium\">Criação de Site:</span> Estamos dedicando nosso tempo e expertise na criação de sites personalizados que refletem a identidade e objetivos de nossos clientes, proporcionando uma presença digital marcante.",
         "<span class=\"font-medium\">Criação de Bot (Discord):</span> Em breve, ofereceremos serviços especializados na criação de bots para Discord, proporcionando soluções de automação e interação para comunidades online.",
@@ -8,48 +13,102 @@ const langs = {
         "<span class=\"font-medium\">Configuração de Servidor FiveM:</span> Em breve, disponibilizaremos serviços de configuração de servidores FiveM, fornecendo soluções personalizadas para comunidades de jogos.",
         "<span class=\"font-medium\">Criação de Scripts FiveM/Bot Discord:</span> Estamos desenvolvendo scripts personalizados para servidores FiveM e bots Discord, oferecendo funcionalidades exclusivas e adaptadas às necessidades de cada projeto.",
         "<span class=\"font-medium\">Configuração de Lojas Virtuais:</span> Preparamos recursos para auxiliar na configuração e personalização de lojas virtuais, possibilitando uma presença online eficiente para vendas."
-      ],
-      "contact": "",
-      "linkNames": {
-        "home": "Página inicial",
-        "services": "Serviços",
-        "contact": "Contato"
-      }
+      ]
     },
-    "Spanish": {
-      "home": "Nos gustaría informar que nuestro sitio web está pasando por un proceso de construcción y mejora. Estamos trabajando arduamente en segundo plano para crear una plataforma en línea que satisfaga sus expectativas y necesidades de la mejor manera posible.",
+    "contact": {
+      "title": "Contato",
+      "text": "Embora nosso site esteja em construção, estamos disponíveis para atendê-lo e responder suas dúvidas através dos seguintes meios de contato:",
+      "tel": "Telefone:",
+      "socialMedia": "Redes sociais:"
+    },
+    "linkNames": {
+      "home": "Página inicial",
+      "services": "Serviços",
+      "contact": "Contato"
+    }
+  },
+  "Spanish": {
+    "home": {
+      "title": "¡Hola, soy Guilherme Lima!",
+      "text": "Nos gustaría informar que nuestro sitio web está pasando por un proceso de construcción y mejora. Estamos trabajando arduamente en segundo plano para crear una plataforma en línea que satisfaga sus expectativas y necesidades de la mejor manera posible."
+    },
+    "services": {
+      "title": "Descripción de los Servicios:",
       "services": [
-  
-      ],
-      "contact": "",
-      "linkNames": {
-        "home": "Página de inicio",
-        "services": "Servicios",
-        "contact": "Contacto"
-      }
+        "<span class=\"font-medium\">Creación de Sitio:</span> Estamos dedicando nuestro tiempo y experiencia en la creación de sitios web personalizados que reflejen la identidad y objetivos de nuestros clientes, proporcionando una presencia digital destacada.",
+        "<span class=\"font-medium\">Creación de Bot (Discord):</span> Próximamente, ofreceremos servicios especializados en la creación de bots para Discord, brindando soluciones de automatización e interacción para comunidades en línea.",
+        "<span class=\"font-medium\">Configuración de Servidor de Discord:</span> Estamos preparando recursos para ayudar en la configuración y optimización de servidores Discord, asegurando una experiencia fluida y eficiente para los usuarios.",
+        "<span class=\"font-medium\">Configuración de Servidor FiveM:</span> Próximamente, ofreceremos servicios de configuración de servidores FiveM, ofreciendo soluciones personalizadas para comunidades de juegos.",
+        "<span class=\"font-medium\">Creación de Scripts FiveM/Bot Discord:</span> Estamos desarrollando scripts personalizados para servidores FiveM y bots Discord, ofreciendo funcionalidades únicas adaptadas a las necesidades de cada proyecto.",
+        "<span class=\"font-medium\">Configuración de Tiendas en Línea:</span> Hemos preparado recursos para ayudar en la configuración y personalización de tiendas en línea, permitiendo una presencia en línea eficiente para ventas."
+      ]
     },
-    "English": {
-      "home": "We would like to inform you that our website is undergoing a construction and enhancement process. We are working diligently behind the scenes to create an online platform that meets your expectations and needs as best as possible.",
+    "contact": {
+      "title": "Contacto",
+      "text": "Aunque nuestro sitio web esté en construcción, estamos disponibles para atenderlo y responder sus preguntas a través de los siguientes medios de contacto:",
+      "tel": "Teléfono:",
+      "socialMedia": "Redes sociales:"
+    },
+    "linkNames": {
+      "home": "Página de inicio",
+      "services": "Servicios",
+      "contact": "Contacto"
+    }
+  },
+  "English": {
+    "home": {
+      "title": "Hello, I'm Guilherme Lima!",
+      "text": "We would like to inform you that our website is undergoing a construction and enhancement process. We are working diligently behind the scenes to create an online platform that meets your expectations and needs as best as possible."
+    },
+    "services": {
+      "title": "Description of Services:",
       "services": [
-  
-      ],
-      "contact": "",
-      "linkNames": {
-        "home": "Home page",
-        "services": "Services",
-        "contact": "Contact"
-      }
+        "<span class=\"font-medium\">Website Creation:</span> We are dedicating our time and expertise to creating custom websites that reflect the identity and goals of our clients, providing a distinctive digital presence.",
+        "<span class=\"font-medium\">Bot Creation (Discord):</span> Coming soon, we will offer specialized services in creating bots for Discord, providing automation and interaction solutions for online communities.",
+        "<span class=\"font-medium\">Discord Server Setup:</span> We are preparing resources to assist in setting up and optimizing Discord servers, ensuring a smooth and efficient experience for users.",
+        "<span class=\"font-medium\">FiveM Server Setup:</span> Coming soon, we will provide FiveM server setup services, offering customized solutions for gaming communities.",
+        "<span class=\"font-medium\">FiveM/Bot Discord Script Creation:</span> We are developing custom scripts for FiveM servers and Discord bots, offering unique functionalities tailored to the needs of each project.",
+        "<span class=\"font-medium\">Online Store Setup:</span> We have prepared resources to assist in the setup and customization of online stores, enabling an efficient online presence for sales."
+      ]
     },
-    "French": {
-      "home": "Nous tenons à vous informer que notre site web est en cours de construction et d'amélioration. Nous travaillons assidûment en coulisses pour créer une plateforme en ligne qui réponde au mieux à vos attentes et besoins.",
+    "contact": {
+      "title": "Contact",
+      "text": "Although our website is under construction, we are available to assist you and answer your questions through the following contact methods:",
+      "tel": "Phone:",
+      "socialMedia": "Social Media:"
+    },
+    "linkNames": {
+      "home": "Home page",
+      "services": "Services",
+      "contact": "Contact"
+    }
+  },
+  "French": {
+    "home": {
+      "title": "Bonjour, je suis Guilherme Lima!",
+      "text": "Nous tenons à vous informer que notre site web est en cours de construction et d'amélioration. Nous travaillons assidûment en coulisses pour créer une plateforme en ligne qui réponde au mieux à vos attentes et besoins."
+    },
+    "services": {
+      "title": "Description des Services:",
       "services": [
-  
-      ],
-      "contact": "",
-      "linkNames": {
-        "home": "Page d'accueil",
-        "services": "Services",
-        "contact": "Contact"
-      }
+        "<span class=\"font-medium\">Création de Site Web:</span> Nous consacrons notre temps et notre expertise à la création de sites web personnalisés qui reflètent l'identité et les objectifs de nos clients, offrant une présence numérique distinctive.",
+        "<span class=\"font-medium\">Création de Bot (Discord):</span> Bientôt, nous proposerons des services spécialisés dans la création de bots pour Discord, offrant des solutions d'automatisation et d'interaction pour les communautés en ligne.",
+        "<span class=\"font-medium\">Configuration de Serveur Discord:</span> Nous préparons des ressources pour aider à la configuration et à l'optimisation des serveurs Discord, garantissant une expérience fluide et efficace pour les utilisateurs.",
+        "<span class=\"font-medium\">Configuration de Serveur FiveM:</span> Bientôt, nous fournirons des services de configuration de serveurs FiveM, offrant des solutions personnalisées pour les communautés de jeux.",
+        "<span class=\"font-medium\">Création de Scripts FiveM/Bot Discord:</span> Nous développons des scripts personnalisés pour les serveurs FiveM et les bots Discord, offrant des fonctionnalités uniques adaptées aux besoins de chaque projet.",
+        "<span class=\"font-medium\">Configuration de Boutiques en Ligne:</span> Nous avons préparé des ressources pour aider à la configuration et à la personnalisation de boutiques en ligne, permettant une présence en ligne efficace pour les ventes."
+      ]
     },
+    "contact": {
+      "title": "Contact",
+      "text": "Bien que notre site soit en construction, nous sommes disponibles pour vous aider et répondre à vos questions par les moyens de contact suivants :",
+      "tel": "Téléphone :",
+      "socialMedia": "Réseaux sociaux :"
+    },
+    "linkNames": {
+      "home": "Page d'accueil",
+      "services": "Services",
+      "contact": "Contact"
+    }
   }
+};
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const { log,debug,table } = console
-
+const $ = document
 /**
  * 
  * @param {HTMLDivElement} div 
@@ -7,12 +7,71 @@ const { log,debug,table } = console
 
 function changeLanguageHome(div) {
   const langSelected = langs[div.id];
-  const covers = document.querySelectorAll("div.cover");
+  const covers = $.querySelectorAll("div.cover");
   const cover = div.querySelector("div.cover");
-  const homeText = document.querySelector("p#home-text");
-  const navs = document.querySelectorAll("a.nav")
+  $.querySelector("p#home-text").textContent = langSelected["home"]["text"];
+  $.querySelector("h1#title").textContent = langSelected["home"]["title"];
+  const navs = $.querySelectorAll("a.nav")
 
-  homeText.textContent = langSelected["home"];
+  covers.forEach(cv => {
+    cv.classList.contains("hidden")? null: cv.classList.add("hidden");
+  })
+  navs.forEach(nav => {
+    nav.textContent = langSelected["linkNames"][nav.id]
+  })
+  cover.classList.remove("hidden");
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} div 
+ */
+
+function changeLanguageServices(div) {
+  const langSelected = langs[div.id];
+  const covers = $.querySelectorAll("div.cover");
+  const cover = div.querySelector("div.cover");
+  const navs = $.querySelectorAll("a.nav");
+  $.querySelector("h3#title").textContent = langSelected["services"]["title"] ;
+  const list = $.querySelector("ul#service-list");
+
+  while(list.firstChild) {
+    list.removeChild(list.firstChild)
+  }
+
+  langSelected["services"]["services"].forEach(service => {
+    const li = $.createElement("li")
+    li.innerHTML = service
+    list.appendChild(li)
+  })
+  covers.forEach(cv => {
+    cv.classList.contains("hidden")? null: cv.classList.add("hidden");
+  })
+  navs.forEach(nav => {
+    nav.textContent = langSelected["linkNames"][nav.id]
+  })
+  cover.classList.remove("hidden");
+}
+/**
+ * 
+ * @param {HTMLDivElement} div 
+ */
+
+function changeLanguageContact(div) {
+  const langSelected = langs[div.id];
+  const covers = $.querySelectorAll("div.cover");
+  const cover = div.querySelector("div.cover");
+  const navs = $.querySelectorAll("a.nav");
+  const title = $.querySelector("h3#title");
+  const text = $.querySelector("p#main-text");
+  const tel = $.querySelector("span#tel");
+  const social = $.querySelector("span#social-media");
+
+  title.textContent = langSelected["contact"]["title"];
+  text.textContent = langSelected["contact"]["text"];
+  tel.textContent = langSelected["contact"]["tel"];
+  social.textContent = langSelected["contact"]["socialMedia"];
+
   covers.forEach(cv => {
     cv.classList.contains("hidden")? null: cv.classList.add("hidden");
   })
